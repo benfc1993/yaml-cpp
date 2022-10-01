e5150831c36855bd36a61821de6717dc6fc6d268
benfc1993 <41189326+benfc1993@users.noreply.github.com>
2022-10-02 00:07:57 +0100

    Create premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,40 @@
+project "yml-cpp"
+	kind "StaticLib"
+	language "C++"
+  staticruntime "off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"src/**.h",
+    "src/**.cpp"
+	}
+
+  includedirs
+  {
+    "include"
+  }
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+    filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+        symbols "off"
